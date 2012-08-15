ecdeb0dae163ff9cee55d039aeabf2d23e948140
Mike P. Kuhl <mkuhl@softmachine.at>
2012-08-15 13:32:00 +0200

    adds resource for less client side compiler

--- grails-app/conf/OfLessGrailsPluginResources.groovy
+++ grails-app/conf/OfLessGrailsPluginResources.groovy
@@ -4,4 +4,9 @@ modules = {
       resource id:'less', url:[plugin: 'of-less-grails', dir:'less/less-grails', file:"application.less"], nominify: true, attrs:[type:'css']
     }
 
+    'less-compiler' {
+      resource id:'less-compiler', url:[plugin: 'of-less-grails', dir:'js/less-grails', file:"less-1.3.0.min.js"],
+              nominify: true, disposition: 'head'
+    }
+
 }
